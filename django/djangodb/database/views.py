3c5ac26b41877027ed521ba17f6c9de74b8595a4
Jorge Costa <Kelho88@pm.me>
2024-05-14 16:12:39 +0100

    changed navbar

--- django/djangodb/database/views.py
+++ django/djangodb/database/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from .models import Colaborator, Event, Evaluation, Criteria
 
 def	home(request):
-	all_users = Colaborator.objects.all
-	return render(request, 'home.html', {'all':all_users})
+	return render(request, 'home.html')
