92d531678201d463fc95777d32d542ab2ab757af
hiimgabe <gabrielgmg2009@gmail.com>
2024-05-14 23:32:05 +0100

    add_user.html fixes

--- django/djangodb/database/views.py
+++ django/djangodb/database/views.py
@@ -26,16 +26,22 @@ def add_user(request):
 	if request.method == 'POST':
 		user_form = UserForm(request.POST)
 		colaborator_form = ColaboratorForm(request.POST)
-		if user_form.is_valid() and colaborator_form.is_valid():
-			user_form.save()
-			colaborator_form.save()
-			return redirect('home')
-		else:
-			user_form = UserForm()
-			colaborator_form = ColaboratorForm()
 
-		context = {
-			'user_form': user_form,
-			'colaborator_form': colaborator_form
-		}
-		return render(request, 'add_user.html', context)
+		if user_form.is_valid():
+			user = user_form.save()
+
+		if colaborator_form.is_valid():
+			colaborator = colaborator_form.save(commit=False)
+			colaborator.id_colaborator = user
+			colaborator.save()
+			return redirect('home')
+
+	user_form = UserForm()
+	colaborator_form = ColaboratorForm()
+
+	context = {
+		'user_form': user_form,
+		'colaborator_form': colaborator_form
+	}
+
+	return render(request, 'add_user.html', context)
