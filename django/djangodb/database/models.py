0d9589ca710170d3d3613898a8d171d5dea8ee75
Gabriel Moreira Gomes <gamoreir@c2r2s6.42porto.com>
2024-05-14 18:59:48 +0100

    add_user page form

--- django/djangodb/database/models.py
+++ django/djangodb/database/models.py
@@ -1,12 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class	Colaborator(models.Model):
-	id_colaborator = models.AutoField(primary_key=True)
+class Colaborator(models.Model):
+	id_colaborator = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 	fname = models.CharField(max_length=100)
 	lname = models.CharField(max_length=100)
 	username = models.CharField(max_length=100)
-	password = models.CharField(max_length=100)
-	tier = models.IntegerField()
 	department = models.CharField(max_length=100)
 	role = models.CharField(max_length=100)
 	admission_date = models.DateField()
@@ -16,7 +15,7 @@ class	Colaborator(models.Model):
 		return self.fname + ' ' + self.lname
 
 class	Event(models.Model):
-	id_event = models.AutoField(primary_key=True)
+	id_event = models.PositiveIntegerField(primary_key=True)
 	beginning = models.DateField()
 	end = models.DateField()
 	staus = models.BooleanField(default=False)
@@ -25,19 +24,19 @@ class	Event(models.Model):
 		return "Evento" + ' ' + self.id_event
 
 class	Evaluation(models.Model):
-	id_evaluation = models.AutoField(primary_key=True)
-	id_evaluator = models.IntegerField()
-	id_evaluated = models.IntegerField()
-	id_event = models.IntegerField()
+	id_evaluation = models.PositiveIntegerField(primary_key=True)
+	id_evaluator = models.ForeignKey(Colaborator, on_delete=models.CASCADE, related_name='evaluations_as_evaluator')
+	id_evaluated = models.ForeignKey(Colaborator, on_delete=models.CASCADE, related_name='evaluations_as_evaluated')
+	id_event = models.ForeignKey(Event, on_delete=models.CASCADE, db_column='id_event')
 	status = models.BooleanField()
 
 	def __str__(self):
 		return "Avaliação" + ' ' + self.id_avaliation
 
-class	Criteria(models.Model):
-	id_evaluation = models.IntegerField()
+class Criteria(models.Model):
+	id_evaluation = models.ForeignKey(Evaluation, on_delete=models.CASCADE, db_column='id_evaluation')
 	question = models.CharField(max_length=200)
-	answer = models.IntegerField()
+	answer = models.PositiveIntegerField()
 	description = models.CharField(max_length=255)
 	comment = models.CharField(max_length=255)
 
