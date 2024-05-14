897a8277ab00b59ba83e08501fb73396f4020989
Jorge Costa <Kelho88@pm.me>
2024-05-14 18:01:16 +0100

    chore: Add new HTML templates for adding events and users

--- django/djangodb/database/urls.py
+++ django/djangodb/database/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	path('', views.home, name="home"),
 	path('add_event/', views.add_event, name="add_event"),
+	path('add_user/', views.add_user, name="add_user"),
 ]
--- django/djangodb/database/views.py
+++ django/djangodb/database/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from .models import Colaborator, Event, Evaluation, Criteria
+from .forms import RenewBookForm
+
 
 def	home(request):
 	return render(request, 'home.html')
 
 def	add_event(request):
 	return render(request, 'add_event.html')
+
+def	add_user(request):
+	return render(request, 'add_user.html')
+
+def add_event(request):
+    form = RenewBookForm()
+    return render(request, 'add_event.html', {'form': form})
